bd30644711fa531ebb8bd8a2ffc268f1fd8bdb6a
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-10-04 12:18:34 +0900

    Update MyFrame.java

---
+++ Basic_Frame/src/MyFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+    public MyFrame(){
+        setTitle("This is a Frame");
+        setSize(400,500);
+        setVisible(true);
+    }
+}
