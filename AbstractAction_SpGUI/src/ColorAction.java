be59c4828ba858c3ad60946f072b69a2ea23dd5b
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-10-14 18:13:00 +0900

    Update ColorAction.java

---
+++ AbstractAction_SpGUI/src/ColorAction.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class ColorAction extends AbstractAction {
+
+    public ColorAction(String name, Icon icon, Color c, Component comp) {
+        putValue(Action.NAME, name);
+        putValue(Action.SMALL_ICON,  icon);
+        putValue("Color", c);
+        target = comp;                      
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Color c = (Color) getValue("Color");
+        target.setBackground(c);
+        target.repaint();
+    }
+
+
+    //data member
+    private Component target;
+}
