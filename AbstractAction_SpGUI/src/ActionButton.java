4734f76196d36fb49f2333c4a934dea0a87ec7d6
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-10-14 18:12:27 +0900

    Update ActionButton.java

---
+++ AbstractAction_SpGUI/src/ActionButton.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class ActionButton extends JButton {
+    public ActionButton(Action a) {
+        setText((String)a.getValue(Action.NAME));
+        Icon icon = (Icon) a.getValue(Action.SMALL_ICON);
+
+        if(icon!=null)
+            setIcon(icon);
+
+        addActionListener(a);
+    }
+}
