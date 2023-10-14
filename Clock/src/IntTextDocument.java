3f34dbe573913f4c09f473be0c14fffac3b77bf5
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-10-14 21:33:07 +0900

    Create IntTextDocument.java

---
+++ Clock/src/IntTextDocument.java
@@ -0,0 +1,18 @@
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+public class IntTextDocument extends PlainDocument {
+    public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+        if(str == null) return;
+
+        String oldString = getText(0,getLength());
+        String newString = oldString.substring(0, offs) + str + oldString.substring(offs);
+
+        try {
+            Integer.parseInt(newString + "0");
+            super.insertString(offs, str, a);
+        } catch (NumberFormatException e) {}
+
+    }
+}
