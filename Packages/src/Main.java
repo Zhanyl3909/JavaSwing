205f00e8ab4c062d429750ba6f56495fc4c59b3f
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-10-20 14:25:02 +0900

    Update Main.java

---
+++ Packages/src/Main.java
@@ -0,0 +1,9 @@
+
+
+import Tools.ToolBox;    //importing the package you've created 
+
+public class Main {
+    public static void main(String[] args) {
+        ToolBox toolBox = new ToolBox();
+    }
+}
